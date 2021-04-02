be5bd34b6d0c8be744ea13c00ed0c48ba45f82fc
Jonathan Fernandes <int3rlop3r@yahoo.in>
2021-04-02 18:59:36 +0530

    ch 3 (pre-freq analysis): can now decrypt the message

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"os"
 )
 
 // challenge: 1
@@ -17,17 +17,18 @@ func hexTo64(s string) (string, error) {
 	return base64.StdEncoding.EncodeToString(data), nil
 }
 
+// challenge: 2
 func xor(buff1, buff2 string) (string, error) {
 	if len(buff1) != len(buff2) {
-		return "", errors.New("strings must be of equal lengths")
+		return "", errors.New("xor: strings must be of equal lengths")
 	}
 	hex1, err := hex.DecodeString(buff1)
 	if err != nil {
-		return "", fmt.Errorf("hexto64: %w", err)
+		return "", fmt.Errorf("xor: %w", err)
 	}
 	hex2, err := hex.DecodeString(buff2)
 	if err != nil {
-		return "", fmt.Errorf("hexto64: %s", err)
+		return "", fmt.Errorf("xor: %w", err)
 	}
 	newBuff := make([]byte, len(hex1), len(hex2))
 	for i := 0; i < len(hex1); i++ {
@@ -36,21 +37,35 @@ func xor(buff1, buff2 string) (string, error) {
 	return hex.EncodeToString(newBuff), nil
 }
 
-func main() {
-	//s := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	//str, err := hexTo64(s)
-	//if err != nil {
-	//fmt.Fprintln(os.Stderr, err)
-	//return
-	//}
-	//fmt.Println(str)
+func nextKey(l int) <-chan []byte {
+	kCh := make(chan []byte)
+	go func() {
+		defer close(kCh)
+		for a := 97; a <= 122; a++ {
+			rep := bytes.Repeat([]byte{byte(a)}, l)
+			kCh <- rep
+		}
+	}()
+	return kCh
+}
 
-	x := "1c0111001f010100061a024b53535009181c"
-	y := "686974207468652062756c6c277320657965"
-	z, err := xor(x, y)
+func xorFreq(s string) {
+	h1, err := hex.DecodeString(s)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Printf("xorFreq: %s\n", err)
 		return
 	}
-	fmt.Println(z)
+	newBuff := make([]byte, len(h1), len(h1))
+	for key := range nextKey(len(h1)) {
+		//fmt.Println("key:", string(key))
+		for i := range key {
+			newBuff[i] = h1[i] ^ key[i]
+		}
+		fmt.Println("xor'ed:", string(newBuff))
+	}
+}
+
+func main() {
+	s := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	xorFreq(s)
 }
